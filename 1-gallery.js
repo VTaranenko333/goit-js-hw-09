ad1778937906e503e7fd6d87ee27aa985676aac9
VTaranenko333 <168170865+VTaranenko333@users.noreply.github.com>
2024-09-05 09:02:54 +0000

    Deploying to gh-pages from @ VTaranenko333/goit-js-hw-09@a02be6e47523b62dbe4851ae417f2b97c70c0391 🚀

---
+++ 1-gallery.js
@@ -0,0 +1,73 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{S as p}from"./assets/vendor-CgTBfC_f.js";const o=[{preview:"https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg",original:"https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg",description:"Hokkaido Flower"},{preview:"https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",description:"Container Haulage Freight"},{preview:"https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",description:"Aerial Beach View"},{preview:"https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",original:"https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",description:"Flower Blooms"},{preview:"https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg",original:"https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg",description:"Alpine Mountains"},{preview:"https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg",description:"Mountain Lake Sailing"},{preview:"https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",description:"Alpine Spring Meadows"},{preview:"https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",description:"Nature Landscape"},{preview:"https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",description:"Lighthouse Coast Sea"}],n=document.querySelector(".gallery"),s=o.map(({preview:t,original:i,description:e})=>`
+      <li class="gallery-item">
+        <a class="gallery-link" href="${i}">
+          <img
+            class="gallery-image"
+            src="${t}"
+            data-source="${i}"
+            alt="${e}"
+          />
+        </a>
+      </li>`).join("");n.insertAdjacentHTML("beforeend",s);new p(".gallery a",{captionsData:"alt",captionDelay:250,captionPosition:"bottom"});const l=`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.gallery {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 24px;
+  list-style: none;
+  margin: 24px auto;
+  padding: 0 156px;
+}
+
+.gallery-item {
+  flex-basis: calc((100% - 72px) / 3);
+}
+
+.gallery-image {
+  width: 100%;
+  height: auto;
+  display: block;
+  transition: transform 0.3s ease-in-out;
+}
+
+.gallery-image:hover {
+  transform: scale(1.05);
+}
+
+.basicLightbox--visible {
+  background: rgba(46, 47, 66, 0.8);
+}
+
+@media (max-width: 1024px) and (min-width: 768px) {
+  .gallery {
+    padding: 0 70px;
+  }
+
+  .gallery-item {
+    flex-basis: calc((100% - 48px) / 2);
+  }
+}
+
+@media (max-width: 767px) {
+  .gallery {
+    padding: 0;
+  }
+
+  .gallery-item {
+    flex-basis: 100%;
+  }
+
+  .gallery {
+    gap: 16px;
+    margin: 16px 16px;
+  }
+}
+`,a=document.createElement("style");a.type="text/css";a.innerText=l;document.head.appendChild(a);
+//# sourceMappingURL=1-gallery.js.map
---
+++ 2-form.js
@@ -0,0 +1,53 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";const o="feedback-form-state";let e={email:"",message:""};const a=document.querySelector(".feedback-form");l();a.addEventListener("input",i);a.addEventListener("submit",n);function i(t){e[t.target.name]=t.target.value,localStorage.setItem(o,JSON.stringify(e))}function n(t){if(t.preventDefault(),e.email=a.elements.email.value.trim(),e.message=a.elements.message.value.trim(),!e.email||!e.message){alert("Fill please all fields");return}console.log(e),a.reset(),localStorage.removeItem(o),e={email:"",message:""}}function l(){const t=localStorage.getItem(o);t&&(e=JSON.parse(t),a.elements.email.value=e.email||"",a.elements.message.value=e.message||"")}const s=`
+  .feedback-form {
+    display: flex;
+    flex-direction: column;
+    border-radius: 8px;
+    padding: 24px;
+    width: 408px;
+    height: auto;
+  }
+
+  .feedback-form input, 
+  .feedback-form textarea {
+    display: flex;
+    flex-direction: column;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 150%;
+    letter-spacing: 0.04em;
+    color: #2e2f42;
+    border: 1px solid #808080;
+    margin-bottom: 8px;
+    border-radius: 4px;
+    width: 360px;
+    height: 40px;
+  }
+
+  .feedback-form input:hover {
+    border: 1px solid #000;
+    border-radius: 4px;
+    width: 360px;
+    height: 40px;
+  }
+
+  .feedback-form button[type="submit"] {
+    align-self: start;
+    border: none;
+    border-radius: 8px;
+    padding: 8px 16px;
+    width: auto;
+    height: 40px;
+    background: #4e75ff;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 150%;
+    letter-spacing: 0.04em;
+    color: #fff;
+  }
+
+  .feedback-form button[type="submit"]:hover {
+    background: #6c8cff;
+  }
+`,r=document.createElement("style");r.type="text/css";r.innerText=s;document.head.appendChild(r);
+//# sourceMappingURL=2-form.js.map
